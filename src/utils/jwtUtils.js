bd541a14b5ae8d2e85f32892624f95b3de40fb7f
TANNEERU CHANDRASHEKAR <149687953+tchandrashekar@users.noreply.github.com>
2026-01-07 15:05:55 +0530

    Update jwtUtils.js

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -1,21 +1,4 @@
-/*export function parseJwt(token){
-    if(!token) return null;
 
-    try{
-        const base64Url =token.spilt(".")[1];
-        const base64 =base64Url.replace(/-/g,"+").replace(/_/g,"/");
-        return JSON.parse(atob(base64));
-    }
-    catch (error) {
-    console.error("Invalid JWT", error);
-    return null;
-    }
-}
-export function getRoleFromToken(token){
-    const payload=parseJwt(token);
-    return payload?.role;
-}
-    */
 
 export function parseJwt(token) {
   if (!token) return null;
