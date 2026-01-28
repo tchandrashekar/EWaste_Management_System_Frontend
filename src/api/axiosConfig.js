de4e7e75bf2056924061404c5008e1dd981054c4
tchandrashekar <tanneeruchandrashekar007@gmail.com>
2026-01-28 19:58:55 +0530

    updated axios base url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api=axios.create({
-    baseURL: "http://localhost:8080",
+   //baseURL: "http://localhost:8080",
+ baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config)=>{
